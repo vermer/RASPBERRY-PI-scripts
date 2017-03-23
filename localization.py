d6b9832a6f11d6337b4537bf6f6d859e926fb9de
Łukasz Makowski <lukasz.makowski@unic.com>
2017-03-23 11:24:54 +0100

    Small modyfication in git ignore and localization.py

--- localization.py
+++ localization.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import re
 import json
-import urllib2
-from urllib2 import urlopen
+import urllib
+from urllib import urlopen
 
 url = 'http://ipinfo.io/json'
 response = urlopen(url)
@@ -15,5 +15,4 @@ country=data['country']
 region=data['region']
 
 if country =='PL':
-    urllib2.urlopen("https://api.telegram.org/bot328753599:AAFmc2WwoKgHZgpoO0FMUlU08Z8oyFhhbFU/sendMessage?chat_id=@RaspberryPILUm&text=" + country +" "+ IP+ " "  + city).read()
-
+    urllib.urlopen("https://api.telegram.org/bot328753599:AAFmc2WwoKgHZgpoO0FMUlU08Z8oyFhhbFU/sendMessage?chat_id=@RaspberryPILUm&text=" + country +" "+ IP+ " "  + city).read()
