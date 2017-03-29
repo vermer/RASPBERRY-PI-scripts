97c7d501891cc9b1455a1536a88ada0c32263794
Łukasz Makowski <lukasz.makowski@unic.com>
2017-03-29 13:48:47 +0200

    Changes in git ignore file.

--- localization.py
+++ localization.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python
-import re
+import ConfigParser
 import json
 import urllib
 from urllib import urlopen
 
+import logging
+logging.basicConfig(filename='localization.log',level=logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
+
+config = ConfigParser.ConfigParser()
+config.read('localization.ini')
 url = 'http://ipinfo.io/json'
 response = urlopen(url)
 data = json.load(response)
 
-IP=data['ip']
-org=data['org']
+IP = data['ip']
+org = data['org']
 city = data['city']
-country=data['country']
-region=data['region']
+country = data['country']
+region = data['region']
 
-if country =='PL':
-    urllib.urlopen("https://api.telegram.org/bot328753599:AAFmc2WwoKgHZgpoO0FMUlU08Z8oyFhhbFU/sendMessage?chat_id=@RaspberryPILUm&text=" + country +" "+ IP+ " "  + city).read()
+message = "Your vpn conection is broken please recconect " + country + " " + IP + " " + city + " " + org + " " + region
+
+if country == 'PL':
+    message = urllib.urlopen(
+        "https://api.telegram.org/bot" +
+        config.get('DEFAULT', 'telegramApiKey') +
+        "/sendMessage?chat_id=" +
+        config.get('DEFAULT', 'telegramUsername') +
+        "&text=" + message
+        ).read()
