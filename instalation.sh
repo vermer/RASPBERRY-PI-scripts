7b629eb3e933763c52622e2f85d92e8bf90e47d7
Łukasz <luk.makowski@gmail.com>
2018-12-09 00:34:01 +0100

    Update instalation.sh

--- instalation.sh
+++ instalation.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo pip install requests
 sudo crontab crontab.bak
+sudo cat /var/log/daily-deluge-backup.log
+sudo cat /var/log/daily-localization-backup.log
+sudo cat /var/log/daily-napi-backup.log
+sudo cat /var/log/daily-update-backup.log
